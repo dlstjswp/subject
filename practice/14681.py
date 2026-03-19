34aa55787ff52c12157425d2ab502e622438c072
dlstjswp <148785069+dlstjswp@users.noreply.github.com>
2026-03-19 15:21:12 +0900

    Create 14681.py

---
+++ practice/14681.py
@@ -0,0 +1,14 @@
+x = int(input())
+y = int(input())
+
+def quadrant(x, y):
+    if x > 0 and y > 0:
+        return 1
+    elif x < 0 and y > 0:
+        return 2
+    elif x < 0 and y < 0:
+        return 3
+    elif x > 0 and y < 0:
+        return 4
+
+print(quadrant(x, y))
