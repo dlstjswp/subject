aea379976fa2c0517d4ac49900637ea70e3b7d21
dlstjswp <148785069+dlstjswp@users.noreply.github.com>
2026-03-25 15:25:43 +0900

    Update 2446.py

--- practice/2/2446.py
+++ practice/2/2446.py
@@ -1,1 +1,16 @@
+num = int(input())
+a = num
+k = 2*a - 1
+m = k
 
+for i in range(num):
+  print(('*' * k).center(m).rstrip())
+  a -= 1
+  k = 2*a - 1
+
+a = 1
+
+for p in range(num-1):
+  a += 1
+  k = 2*a - 1 
+  print(('*' * k).center(m).rstrip())
