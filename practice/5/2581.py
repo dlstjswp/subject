598b692d081e538a474109583686fc4c7a82f571
dlstjswp <148785069+dlstjswp@users.noreply.github.com>
2026-04-08 20:21:33 +0900

    Update 2581.py

--- practice/5/2581.py
+++ practice/5/2581.py
@@ -1,8 +1,15 @@
 def prime_num(x):
-    for i in range(2, round(x**(1/2)) + 2):
-        if x%i == 0:
-            return 0
-    return x
+  if x == 1:
+    return 0
+  if x == 2:
+    return 2
+  if x == 3:
+    return 3
+  else:
+      for i in range(2, round(x**(1/2)) + 2):
+          if x%i == 0:
+              return 0
+      return x
 
 
 
@@ -13,9 +20,8 @@ p = []
 for j in range(m, n+1):
      p.append(prime_num(j))
 
-
-p = list(set(p))
-p.remove(0)
+p = [k for k in p if k !=0]
+      
 
 if len(p) != 0:
     print(sum(p))
